7e925ba0419af4487e7a087d480fcfba4bc1e535
leviviana21 <viana.levi22@gmail.com>
2026-03-04 14:05:59 -0300

    Atualizando pom.xml, EmailService e application.properties

--- demo/src/main/java/leituratest/demo/EmailService.java
+++ demo/src/main/java/leituratest/demo/EmailService.java
@@ -1,13 +1,18 @@
 package leituratest.demo;
 
 
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
+    @Autowired
+    private JavaMailSender javaMailSender;
 
-    public JavaMailSender
+        public String enviarEmailTexto (String destinatario , String mensagem) {
 
 
+        }
 }

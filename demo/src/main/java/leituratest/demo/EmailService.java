9d18fb1041beb79d3d41c0602c53368e310deef9
leviviana21 <viana.levi22@gmail.com>
2026-03-04 14:36:40 -0300

    Atualizando o EmailSender

--- demo/src/main/java/leituratest/demo/EmailService.java
+++ demo/src/main/java/leituratest/demo/EmailService.java
@@ -3,6 +3,8 @@ package leituratest.demo;
 
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
@@ -11,8 +13,19 @@ public class EmailService {
     @Autowired
     private JavaMailSender javaMailSender;
 
-        public String enviarEmailTexto (String destinatario , String mensagem) {
+    @Value("${spring.mail.username}")
+    private String remetente;
 
-
+        public String enviarEmailTexto (String destinatario , String mensagem , String assunto) {
+            try {
+                SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+                simpleMailMessage.setFrom(remetente);
+                simpleMailMessage.setFrom(mensagem);
+            }catch ()
         }
+
+
+
+
+
 }
